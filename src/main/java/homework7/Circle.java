263acb0069d83cd0eee4839dcf534aec227b57d8
Irakli Barbakadze <ibarbakadze@credo.ge>
2026-06-06 21:23:59 +0400

    [Irakli B.] HW7 - Circle implemets Shape

---
+++ src/main/java/homework7/Circle.java
@@ -0,0 +1,17 @@
+/**@author Irakli Barbakadze*/
+package homework7;
+
+public class Circle implements Shape{
+    private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+}
