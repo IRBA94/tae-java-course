bf1272c43ec2b32f53228e4343dcca2d064b0560
Irakli Barbakadze <ibarbakadze@credo.ge>
2026-06-06 22:24:30 +0400

    [Irakli B.] HW7 - Square implements Drawable

---
+++ src/main/java/homework7/Square.java
@@ -0,0 +1,10 @@
+/**@author Irakli Barbakadze*/
+package homework7;
+
+public class Square implements Drawable {
+
+    @Override
+    public void draw() {
+    System.out.println("Drawing Square");
+    }
+}
