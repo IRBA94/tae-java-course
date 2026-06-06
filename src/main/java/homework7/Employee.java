59f0668921091e56e8c465eeb7cd86d362662c55
Irakli Barbakadze <ibarbakadze@credo.ge>
2026-06-06 23:47:44 +0400

    [Irakli B.] HW7 - abstract class Employee

---
+++ src/main/java/homework7/Employee.java
@@ -0,0 +1,28 @@
+/**@author Irakli Barbakadze*/
+package homework7;
+
+public abstract class Employee {
+    protected String name;
+
+    public Employee(String name){
+        this.name=name;
+    }
+
+    abstract double calculateSalary();
+
+    void printInfo(){
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Employee[] team = {
+                new FullTime("Anna", 5000),
+                new PartTime("Beka", 25, 80) };
+
+        for (Employee e : team) {
+            e.printInfo();
+            System.out.println(e.calculateSalary());
+        }
+    }
+
+}
